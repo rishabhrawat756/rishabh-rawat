9feeaccbfb39e42be368013edfe13c2b891c6cf6
rishabhrawat756 <71601042+rishabhrawat756@users.noreply.github.com>
2020-09-22 16:52:47 +0530

    Create positive_numbers.py

---
+++ positive_numbers.py
@@ -0,0 +1,12 @@
+# rishabh-rawat
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+print("list1=",list1)
+for i in list1:
+    if i>=0:
+        print(i,end=" ")
+print(end="\n")
+print("list2=",list2)
+for j in list2:
+    if j>=0:
+        print(j,end=" ")
